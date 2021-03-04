671a19fa96d20aeebdf4168606c9366651c1d31b
Kelsie Besinger <kelsiebesinger@gmail.com>
2021-03-04 13:45:50 -0700

    Add links to card display on homepage

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Home = () => {
     return (
-        <>
+      <>
         <article className="aboutSection">
           <h1 className="mainPageText header">CryptoWatch</h1>
           <h2 className="mainPageText">
@@ -10,19 +11,34 @@ const Home = () => {
           </h2>
           <h3 className="mainPageText">Mission Statement</h3>
         </article>
-        <div className='sectionContainer'>
-          <article className="beginnerSection mainSection">
-            <p className='cardText'>I'm new to crypto. I need some explanation on how everything works.</p>
-          </article>
-          <article className="curiousSection mainSection">
-            <p className='cardText'>I'm curious about crypto. I would like to view live prices and how the market is moving.</p>
-          </article>
-          <article className="buySection mainSection">
-            <p className='cardText'>I'm going to invest in crypto. I'm looking for a exchange that can help me buy crypto.</p>
-          </article>
+        <div className="sectionContainer">
+          <Link to="cryptopedia">
+            <article className="beginnerSection mainSection">
+              <p className="cardText">
+                I'm new to crypto. I need some explanation on how everything
+                works.
+              </p>
+            </article>
+          </Link>
+          <Link to="cryptoMarkets">
+            <article className="curiousSection mainSection">
+              <p className="cardText">
+                I'm curious about crypto. I would like to view live prices and
+                how the market is moving.
+              </p>
+            </article>
+          </Link>
+          <Link to="cryptoExchanges">
+            <article className="buySection mainSection">
+              <p className="cardText">
+                I'm going to invest in crypto. I'm looking for a exchange that
+                can help me buy crypto.
+              </p>
+            </article>
+          </Link>
         </div>
-        </>
-    )
+      </>
+    );
 }
 
 export default Home
