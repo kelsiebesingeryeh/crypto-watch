83c2a47f4c203c97a558c67ab172896f24fdbf8c
Kelsie Besinger <kelsiebesinger@gmail.com>
2021-03-04 08:42:48 -0700

    Add styled stock market price

--- src/Components/CryptocurrenciesCard/CryptocurrenciesCard.js
+++ src/Components/CryptocurrenciesCard/CryptocurrenciesCard.js
@@ -9,18 +9,21 @@ const CryptocurrenciesCard = ({
   price,
   marketCap,
 }) => {
-  const stylePrice = price.toFixed(2);
+  const stylePrice = price.toFixed(2).replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+  const styleMarketCap = Math.abs(Number(marketCap)) >= 1.0e9;
 
   return (
     <div className="cryptoCards">
       <span className="cardStyling">
-        <p className="cardInfo">
-          {name}, {symbol}
-        </p>
         <p className="cardInfo rank">{rank}</p>
+        <p className="cardInfo">
+          {name} <strong>({symbol})</strong>
+        </p>
       </span>
-      <p>Current Price: ${stylePrice}</p>
-      <p>Market Cap: {marketCap}</p>
+      <p className="price">
+        <strong>${stylePrice}</strong>
+      </p>
+      <p>Market Cap: {styleMarketCap}</p>
     </div>
   );
 };
