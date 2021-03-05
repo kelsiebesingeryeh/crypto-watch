4328cd94da74466a646e81093d06d13dfa3d0d75
Kelsie Besinger <kelsiebesinger@gmail.com>
2021-03-04 20:37:54 -0700

    Add table headers

--- src/Components/Cryptocurrencies/Cryptocurrencies.js
+++ src/Components/Cryptocurrencies/Cryptocurrencies.js
@@ -1,16 +1,7 @@
-import { render } from '@testing-library/react'
 import React, {Component} from 'react'
 import CryptocurrenciesCard from "../CryptocurrenciesCard/CryptocurrenciesCard"
 import './Cryptocurrencies.css'
 import { getAllCoins } from '../../apiCalls'
-import { makeStyles } from "@material-ui/core/styles"
-import Table from "@material-ui/core/Table"
-import TableBody from "@material-ui/core/TableBody"
-import TableCell from "@material-ui/core/TableCell"
-import TableContainer from "@material-ui/core/TableContainer"
-import TableHead from "@material-ui/core/TableHead"
-import TableRow from "@material-ui/core/TableRow"
-import Paper from "@material-ui/core/Paper"
 
 class Cryptocurrencies extends Component {
     constructor() {
@@ -50,8 +41,14 @@ class Cryptocurrencies extends Component {
     render() {
       return (
         <>
-          <table>
+          <table className='cryptoTable'>
             <tbody>
+              <th>Rank</th>
+              <th>Cryptocurrency</th>
+              <th>Symbol</th>
+              <th>Price</th>
+              <th>24HR%Chg</th>
+              <th>Market Cap</th>
             {this.cryptocurrenciesOnDisplay()}
             </tbody>
           </table>
