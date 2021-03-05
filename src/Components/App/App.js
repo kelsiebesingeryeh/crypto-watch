8b3b619fd0b2a33b7e9645f242924d449ebff797
Kelsie Besinger <kelsiebesinger@gmail.com>
2021-03-05 10:35:18 -0700

    Change App back into class component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,5 +1,5 @@
 import './App.css'
-import React from 'react'
+import React, { Component } from 'react'
 import Cryptocurrencies from '../Cryptocurrencies/Cryptocurrencies'
 import Nav from '../Nav/Nav'
 import {Route} from 'react-router-dom'
@@ -7,34 +7,42 @@ import Home from '../Home/Home'
 import CryptocurrencyDetails from '../CryptocurrencyDetails/CryptocurrencyDetails'
 import { getAllCoins } from "../../apiCalls"
 
-const App = () => {
-    return (
-      <main>
-        <Nav />
-        <Route exact path="/" component={Home} />
-        <Route path="/cryptocurrencies" component={Cryptocurrencies} />
-        <Route
-          exact
-          path={"/cryptocurrencies/:id"}
-          render={({ match }) => {
-            const id = parseInt(match.params.id)
-            return (
-              <div className='cryptocurrencyDetailsContainer'>
-                <CryptocurrencyDetails id={id}/>
-              </div>
-            )
-          }
-        }
-        />
-      </main>
-    );
-
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      cryptocurrencies: []
+    }
   }
 
-export default App;
+  componentDidMount() {
+      getAllCoins().then((cryptocurrencies) =>
+        this.setState({ cryptocurrencies })
+      )
+    }
 
-// componentDidMount() {
-//     getAllCoins().then((cryptocurrencies) =>
-//       this.setState({ cryptocurrencies })
-//     )
-//   }
+    render() {
+      return (
+        <main>
+          <Nav />
+          <Route exact path="/" component={Home} />
+          <Route path="/cryptocurrencies" component={() => <Cryptocurrencies cryptocurrencies={this.state.cryptocurrencies}/>} />
+          <Route
+            exact
+            path={"/cryptocurrencies/:id"}
+            render={({ match }) => {
+              const id = parseInt(match.params.id)
+              return (
+                <div className='cryptocurrencyDetailsContainer'>
+                  <CryptocurrencyDetails id={id}/>
+                </div>
+              )
+            }
+          }
+          />
+        </main>
+      )
+    }
+  }
+
+export default App
