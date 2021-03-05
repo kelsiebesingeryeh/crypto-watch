79271f1aca497adaab1db7de111a6cad621c5958
Kelsie Besinger <kelsiebesinger@gmail.com>
2021-03-05 11:23:36 -0700

    Add functionality to render tags, teams and links dynamically

--- src/Components/CryptocurrencyDetails/CryptocurrencyDetails.js
+++ src/Components/CryptocurrencyDetails/CryptocurrencyDetails.js
@@ -15,11 +15,41 @@ class CryptocurrencyDetails extends Component {
      .then(currentCoin => this.setState({currentCoin}))
     }
 
+    linkItems() {
+      return this.state.currentCoin.links.explorer.map(coin => {  
+        return <li>{coin}</li>
+      })
+    }
+
+    tagItems() {
+        return this.state.currentCoin.tags.map(coin => {
+            return <li>{coin.name}</li>
+        })
+    }
+
+    teamItems() {
+        return this.state.currentCoin.team.map(coin => {
+            return <li>{coin.name}, {coin.position}</li>
+        })
+    }
+
     render() {
         return (
-          <section className="coinDetails">
-            {this.state.currentCoin && <p>{this.state.currentCoin.name}</p>}
-          </section>
+          <>
+            {this.state.currentCoin && (
+              <section className="coinDetails">
+                <h1>{this.state.currentCoin.name}</h1>
+                <h2>Price</h2>
+                <p>{this.state.currentCoin.description}</p>
+                <ul>Helpful Links</ul>
+                {this.linkItems()}
+                <ul>Tags</ul>
+                {this.tagItems()}
+                <ul>Team</ul>
+                {this.teamItems()}
+              </section>
+            )}
+          </>
         );
 
     }
