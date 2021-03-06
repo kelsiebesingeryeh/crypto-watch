c0544be9ff9e523080cc2a91f48694be39577569
Kelsie Besinger <kelsiebesinger@gmail.com>
2021-03-06 10:30:21 -0700

    Add tests for navbar icons

---
+++ cypress/integration/crypto_watch_spec.js
@@ -0,0 +1,14 @@
+describe('Home', () => {
+    const baseURL = "http://localhost:3000/"
+
+it('Should see a home button in the nav bar when a user visits the homepage', () => {
+    cy.visit(baseURL)
+    .get(".navIcon").should('be.visible')
+})
+
+it('Should see menu items in the nav bar when a user visits the homepage', () => {
+    cy.visit(baseURL)
+    .get(".rightNavItems").should('contain', 'Cryptopedia', 'Markets', 'Exchanges', 'My Watch List')
+})
+
+})
