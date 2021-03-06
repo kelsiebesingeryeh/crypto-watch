05a97f00925d045810992d815a081cb92eaad47b
Kelsie Besinger <kelsiebesinger@gmail.com>
2021-03-06 11:29:14 -0700

    Add class component for exchanges

---
+++ src/Components/Exchanges/Exchange.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class Exchanges extends Component {
+    constructor() {
+        super()
+        this.state = {
+            exchanges: []
+        }
+    }
+
+    render() {
+        <h1>Exchanges</h1>
+    }
+}
+
+export default Exchanges
