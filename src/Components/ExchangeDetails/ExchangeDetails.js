b20de5ea6b972d673e39c525c1318b51189056af
Kelsie Besinger <kelsiebesinger@gmail.com>
2021-03-06 12:27:48 -0700

    Add fiats to display

--- src/Components/ExchangeDetails/ExchangeDetails.js
+++ src/Components/ExchangeDetails/ExchangeDetails.js
@@ -3,6 +3,7 @@ import React from 'react'
 const ExchangeDetails = ({ name, links, currencies, markets, fiats, volume, confidenceScore }) => {
     const exchangePercentage = confidenceScore * 10
     const styledVolume = volume.toFixed(0).replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")
+    const displayFiats = fiats.map(fiat => fiat.symbol).join(', ')
     return (
       <tr>
         <td>{name}</td>
@@ -10,9 +11,9 @@ const ExchangeDetails = ({ name, links, currencies, markets, fiats, volume, conf
         <td>${styledVolume}</td>
         <td>{markets}</td>
         <td>{currencies}</td>
+        <td>{displayFiats}</td>
       </tr>
-    );
+    )
 }
 
 export default ExchangeDetails
-{/* <td>{fiats}</td> */}
