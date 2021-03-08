fe81f28aea2f2fffd844d910dd7511b5f62d5fe9
Kelsie Besinger <kelsiebesinger@gmail.com>
2021-03-07 18:31:09 -0700

    Add error and loading to Cryptopedia Component

--- src/Components/Cryptopedia/Cryptopedia.js
+++ src/Components/Cryptopedia/Cryptopedia.js
@@ -1,10 +1,9 @@
-import React, { Component } from 'react'
-import {getAllTags} from '../../apiCalls'
+import React from 'react'
 import Loading from '../Loading/Loading'
 import Error from '../Error/Error'
 import CryptopediaTags from '../CryptopediaTags/CryptopediaTags'
 
-const Cryptopedia = ({tags, isLoading, error}) => {
+const Cryptopedia = ({tags, loading, error}) => {
 
 const tagsOnDisplay = () => {
     const sortedTags = tags.sort((a, b) => {  
@@ -24,60 +23,19 @@ const tagsOnDisplay = () => {
         )
     })
    }
+
+    if (loading && !tags.length) {
+        return <Loading />
+    } else if (error && tags.length) {
+        return <Error />
+    } else if (tags.length) {
         return (
             <>
             <h1>Crypto 101</h1>
             {tagsOnDisplay()}
             </>
         )
+    }
 }
 
 export default Cryptopedia
-
-
-// const Exchanges = ({ exchanges, isLoading, error }) => {
-//   const exchangesOnDisplay = () => {
-//     const sortedExchange = exchanges.sort(
-//       (a, b) => b["confidence_score"] - a["confidence_score"]
-//     );
-//     return sortedExchange.map((exchange) => {
-//       return (
-//         <ExchangeDetails
-//           key={exchange.id}
-//           id={exchange.id}
-//           name={exchange.name}
-//           links={exchange.links}
-//           currencies={exchange.currencies}
-//           markets={exchange.markets}
-//           fiats={exchange.fiats}
-//           volume={exchange.quotes.USD["reported_volume_24h"]}
-//           confidenceScore={exchange["confidence_score"]}
-//         />
-//       );
-//     });
-//   };
-//   return (
-//     <>
-//       {isLoading && <Loading />}
-//       {error && <Error />}
-//       <div className="cryptoTableContainer">
-//         <p className="cryptoTableHeading">Cryptocurrency Exchanges</p>
-//         <table className="cryptoTable">
-//           <thead>
-//             <tr>
-//               <th>Exchange Name</th>
-//               <th>Exchange Score</th>
-//               <th>Volume(24H)</th>
-//               <th># Markets</th>
-//               <th># Coins</th>
-//               <th>Fiats Supported</th>
-//             </tr>
-//           </thead>
-//           <tbody>{exchangesOnDisplay()}</tbody>
-//         </table>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default Exchanges;
