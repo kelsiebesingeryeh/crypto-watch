8d9982da43c369246840750ca94b58b7ceb4af28
Kelsie Besinger <kelsiebesinger@gmail.com>
2021-06-14 11:14:15 -0600

    Add useEffect to get the filtered favorites array and setState

--- src/Components/Favorites/Favorites.js
+++ src/Components/Favorites/Favorites.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 
 
-const Favorites = () => {
+const Favorites = ({ cryptocurrencies, favorites }) => {
+    console.log('crypto', cryptocurrencies);
+    console.log('favs', favorites);
+    const [myFavorites, setMyFavorites] = useState([]);
+
+    // console.log('work', filteredFavorites);
+    
+    useEffect(() => {
+        const filteredFavorites = cryptocurrencies.filter((coin) => favorites.includes(coin.id));
+        setMyFavorites(filteredFavorites);
+    }, []);
+
+    console.log(myFavorites);
+
     return (
         <>
             <h3>My Favorites coming soon</h3>
@@ -10,3 +24,8 @@ const Favorites = () => {
 };
 
 export default Favorites;
+
+Favorites.propTypes = {
+    cryptocurrencies: PropTypes.array,
+    favorites: PropTypes.array,
+};
